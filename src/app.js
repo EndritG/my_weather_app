422be796d4ba6e4cb8aeb1c002229841e372fc73
unknown <endrit.germizaj2@gmail.com>
2022-04-13 10:09:20 +0200

    Final

--- src/app.js
+++ src/app.js
@@ -5,6 +5,24 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast');
 
 const app = express();
+
+// const mysql = require('mysql');
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'dbuser',
+//   password: 's3kreee7',
+//   database: 'my_db',
+// });
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+// connection.end();
 //  Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
 const viewsPath = path.join(__dirname, '../templates/views');
